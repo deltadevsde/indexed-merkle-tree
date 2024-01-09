1e653204144b76461cd8b62b6842fe50da602a4a
sebasti810 <spusch@outlook.de>
2024-01-09 15:40:07 +0100

    feat: create own crate for indexed merkle tree

---
+++ src/error.rs
@@ -0,0 +1,17 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum MerkleTreeError {
+    #[error("{0} not found")]
+    NotFoundError(String),
+    #[error("Failed to order merkle tree nodes")]
+    OrderingError,
+    #[error("The Merkle tree is empty")]
+    EmptyMerkleTreeError,
+    #[error("Failed to retrieve the node at index {0}")]
+    IndexError(String),
+    #[error("Invalid format error: {0}")]
+    InvalidFormatError(String),
+    #[error("Failed to generate Merkle proof")]
+    MerkleProofError
+}
